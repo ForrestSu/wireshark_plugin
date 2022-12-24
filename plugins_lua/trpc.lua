0f6fc18a0f1df44c1022b9d42aeead4f6191acd5
sunquan <sunquana@gmail.com>
2022-12-24 23:35:48 +0800

    feat: trpc

--- plugins_lua/trpc.lua
+++ plugins_lua/trpc.lua
@@ -2,25 +2,37 @@
 -- @date   2022.12.24
 -- @doc    https://wiki.wireshark.org/LuaAPI
 
-local ProtoTypeText = {
-    [0x0] = "Unknown",
-    [0x1] = "Protocol Buffer",
-    [0x2] = "JSON"
+local VersionText = {
+    [0x0] = "V0",
+    [0x1] = "V1",
+}
+
+local FrameTypeText = {
+    [0x0] = "UNARY_FRAME",  -- 一应一答模式的二进制数据帧类型
+    [0x1] = "STREAM_FRAME", -- 流式模式的二进制数据帧类型
+}
+
+local StreamFrameTypeText = {
+    [0x0] = "UNARY",
+    [0x1] = "INIT",
+    [0x2] = "DATA",
+    [0x3] = "FEEDBACK",
+    [0x4] = "CLOSE",
 }
 
 -- 1. Create Protocol Object
 local NAME = "tRPC"
-local trpcProto = Proto(NAME, "Tencent TRPC Protocol")
+local trpcProto = Proto(NAME, "TRPC Protocol")
 
 -- 2. Define Protocol Fields
 local fields = trpcProto.fields
 fields.Magic = ProtoField.uint16(NAME .. ".Magic", "Magic", base.HEX)
-fields.FrameType = ProtoField.uint8(NAME .. ".FrameType", "FrameType", base.DEC)
-fields.StreamFrameType = ProtoField.uint8(NAME .. ".StreamFrameType", "StreamFrameType", base.DEC)
+fields.FrameType = ProtoField.uint8(NAME .. ".FrameType", "FrameType", base.DEC, FrameTypeText)
+fields.StreamFrameType = ProtoField.uint8(NAME .. ".StreamFrameType", "StreamFrameType", base.DEC, StreamFrameTypeText)
 fields.TotalLen = ProtoField.uint32(NAME .. ".TotalLen", "TotalLen", base.DEC)
 fields.HeaderLen = ProtoField.uint16(NAME .. ".HeaderLen", "HeaderLen", base.DEC)
 fields.StreamID = ProtoField.uint32(NAME .. ".StreamID", "StreamID", base.DEC)
-fields.Version = ProtoField.uint8(NAME .. ".Version", "Version", base.DEC)
+fields.Version = ProtoField.uint8(NAME .. ".Version", "Version", base.DEC, VersionText)
 fields.Reserved = ProtoField.uint8(NAME .. ".Reserved", "Reserved", base.DEC)
 fields.Header = ProtoField.string(NAME .. ".Header", "Header", base.ASCII)
 fields.Body = ProtoField.string(NAME .. ".Body", "Body", base.ASCII)
@@ -71,7 +83,7 @@ function tRPCdissector(buffer, pinfo, tree)
     subtree:add(fields.Header, header)
 
     local totalLen = buffer(4, 4):uint()
-    local body = buffer(16+headerLen, totalLen - headerLen - 16)
+    local body = buffer(16 + headerLen, totalLen - headerLen - 16)
     subtree:add(fields.Body, body)
 
     -- 3.4 finally Info
@@ -92,6 +104,27 @@ function trpcProto.dissector(buffer, pinfo, tree)
     end
 end
 
+local function heuristic_checker(buffer, pinfo, tree)
+    -- guard for length
+    local length = buffer:len()
+    if length < 8 then
+        return false
+    end
+
+    local magic = buffer(0, 2):uint()
+    if magic == 0x930 then
+        -- 检查是否分段
+        local pdu_length = buffer(4, 4):uint()
+        if pdu_length > buffer:len() then
+            pinfo.desegment_len = pdu_length - buffer:len()
+        else
+            trpcProto.dissector(buffer, pinfo, tree)
+        end
+        return true
+    else
+        return false
+    end
+end
+
 -- 4. Register decoder to wireshark
-local tcp_port_table = DissectorTable.get("tcp.port")
-tcp_port_table:add(8009, trpcProto)
+trpcProto:register_heuristic("tcp", heuristic_checker)
